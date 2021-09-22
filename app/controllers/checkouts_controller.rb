4ac9a9a1a415b6e2851eeedc88bbfa854761650f
PascalKoeimanHHS <19142617@student.hhs.nl>
2021-09-22 11:45:20 +0200

    remove puts statement from checkoutscontroller

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,9 +1,5 @@
 class CheckoutsController < ApplicationController
-    def create
-        # Check whether this is a secure/stable way to handle the API key
-        # Stripe.api_key = Rails.application.credentials[:stripe][:secret]
-        puts current_user.stripe_customer_id
-        
+    def create     
         @webinar = Webinar.find(params[:id])
         @session = Stripe::Checkout::Session.create({
             # Passing in the currently logged in customer during checkout fills in info like the email automatically
